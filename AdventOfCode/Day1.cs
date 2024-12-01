fd1ffdc935be6a324a02c440ca2e29d48c71c691
Vinny Keeler <101831569+ShieldedAsh@users.noreply.github.com>
2024-12-01 17:39:45 -0500

    Commented Day1.cs

--- AdventOfCode/Day1.cs
+++ AdventOfCode/Day1.cs
@@ -42,10 +42,12 @@ public class Day1
             System.Console.WriteLine($"An error occured: {ex.Message}");
         }
         
+        //sort both lists
         leftSide.Sort();
         rightSide.Sort();
 
         int sum = 0;
+        
         //find distances and sum
         for (int i = 0; i < leftSide.Count; i++)
         {
@@ -61,22 +63,30 @@ public class Day1
             }
         }
 
+        //Part 2
+
+        //declare similarity and multiplier values to be iterated over
         int similarity = 0;
         int multiplier = 0;
+
+        //check each left side value and see how many matches there are on the right side
         for (int i = 0; i < leftSide.Count; i++)
         {
             int numToCheck = leftSide[i];
             multiplier = 0;
             for (int j = 0; j < rightSide.Count; j++)
             {
+                //increase multiplier for each match
                 if (numToCheck == rightSide[j])
                 {
                     multiplier++;
                 }
             }
+            //increase similarity based on how many times the value appears in the list
             similarity += (numToCheck * multiplier);
         }
 
+        //print out solutions
         System.Console.WriteLine(sum);
         System.Console.WriteLine(similarity);
     }
